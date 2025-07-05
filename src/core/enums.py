cd8743b584f261f2e450b4d724a883f454004a97
Jesse Boudreau <jesseboudreau@yahoo.com>
2025-07-05 15:01:45 +0000

    fix(enums): include GPT-4o in AllModelEnum

--- src/core/enums.py
+++ src/core/enums.py
@@ -20,6 +20,7 @@ class DatabaseType(str, Enum):
     POSTGRES = "postgres"
 
 class AllModelEnum(str, Enum):
+    GPT_4O              = "gpt-4o"
     GPT_4O_MINI         = "gpt-4o-mini"
     GPT_3_5_TURBO       = "gpt-3.5-turbo"
     OPENAI_COMPATIBLE   = "openai-compatible"
